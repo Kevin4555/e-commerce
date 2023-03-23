1e01096bc53f3850c02a535aa9189df23d60da6f
nicoroke <nicoroke@gmail.com>
2023-03-23 14:23:51 -0300

    Nico
    Product Layout

--- src/components/pages/Product/Product.jsx
+++ src/components/pages/Product/Product.jsx
@@ -1,13 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Carousel from "react-bootstrap/Carousel";
 import PageNavbar from "../../navbar/PageNavbar";
 import "./Product.css";
 
 function Product() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await axios({
+          method: "get",
+          url: `http://localhost:8000/products/${id}`,
+        });
+        console.log(response.data);
+        setProduct(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProduct();
+  }, [id]);
   return (
     <>
       <PageNavbar />
       <body>
-        <div className="row d-flex justify-content-center mt-5">
+        <div className="d-flex justify-content-center mt-5">
           <div className="carousel-div me-5">
             <Carousel>
               <Carousel.Item>
@@ -55,17 +76,46 @@ function Product() {
             </Carousel>
           </div>
 
-          <div className="d-flex align-items-center carousel-div">
+          <div className="d-flex align-items-center carousel-div px-0">
             <div>
-              <h1 className="">Titulo</h1>
-              <small className="d-block mb-3 mt-5">USD 84</small>
-              <small className="d-block my-3">Estrellitas</small>
-              <small className="d-block my-3">Stock: 12312312 units.</small>
+              <h1 className="">
+                {product ? (
+                  product.title
+                ) : (
+                  <img src="/img/Spinner-1s-200px.svg" alt="" />
+                )}
+              </h1>
+              <small className="d-block mb-3 mt-5">
+                {product ? (
+                  product.price
+                ) : (
+                  <img src="/img/Spinner-1s-200px.svg" alt="" />
+                )}{" "}
+                USD
+              </small>
+              <small className="d-block my-3">
+                {" "}
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+              </small>
+              <small className="d-block my-3">
+                Stock:{" "}
+                {product ? (
+                  product.stock
+                ) : (
+                  <img src="/img/Spinner-1s-200px.svg" alt="" />
+                )}{" "}
+                units.
+              </small>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem illum dolor, reprehenderit hic nulla, ad quod fugit
-                quis maxime nemo veritatis dolorem beatae excepturi asperiores
-                molestiae sunt. Eaque, dolore aspernatur?
+                {product ? (
+                  product.description
+                ) : (
+                  <img src="/img/Spinner-1s-200px.svg" alt="" />
+                )}
               </p>
               <div className="">
                 <button className="btn fw-semibold" id="btn">
@@ -76,6 +126,90 @@ function Product() {
                   <i className="bi bi-heart"></i>
                 </button>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5 mx-auto" id="reviews-container">
+          <h3>Comentarios</h3>
+          <div className="mb-4">
+            <small>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+            </small>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+              ducimus illum nulla placeat ab, rem excepturi quibusdam distinctio
+              earum. Voluptate facere suscipit ut fugit illo molestias! Ratione,
+              quasi. Doloribus, delectus?
+            </p>
+            <div>
+              <img
+                src="/img/default-avatar.jpg"
+                alt="User img"
+                className="user-avatar rounded-pill"
+              />
+              <small className="mx-2 fw-semibold">Pepe Rodriguez</small>
+              <small>
+                <small>21 mar 2023 </small>
+              </small>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <small>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+            </small>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+              ducimus illum nulla placeat ab, rem excepturi quibusdam distinctio
+              earum. Voluptate facere suscipit ut fugit illo molestias! Ratione,
+              quasi. Doloribus, delectus?
+            </p>
+            <div>
+              <img
+                src="/img/default-avatar.jpg"
+                alt="User img"
+                className="user-avatar rounded-pill"
+              />
+              <small className="mx-2 fw-semibold">Pepe Rodriguez</small>
+              <small>
+                <small>21 mar 2023 </small>
+              </small>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <small>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+              <i class="bi bi-star-fill"></i>
+            </small>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+              ducimus illum nulla placeat ab, rem excepturi quibusdam distinctio
+              earum. Voluptate facere suscipit ut fugit illo molestias! Ratione,
+              quasi. Doloribus, delectus?
+            </p>
+            <div>
+              <img
+                src="/img/default-avatar.jpg"
+                alt="User img"
+                className="user-avatar rounded-pill"
+              />
+              <small className="mx-2 fw-semibold">Pepe Rodriguez</small>
+              <small>
+                <small>21 mar 2023 </small>
+              </small>
             </div>
           </div>
         </div>
