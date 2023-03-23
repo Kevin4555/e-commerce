58ecd5abba25fabe95981b1fcda69ac32754113f
nicoroke <nicoroke@gmail.com>
2023-03-23 11:07:18 -0300

    Nico
    Parte de Layouts: Home, Product

--- src/components/pages/Product/Product.jsx
+++ src/components/pages/Product/Product.jsx
@@ -1,13 +1,14 @@
 import Carousel from "react-bootstrap/Carousel";
 import PageNavbar from "../../navbar/PageNavbar";
+import "./Product.css";
 
 function Product() {
   return (
     <>
       <PageNavbar />
       <body>
-        <div className="row">
-          <div className="col-6">
+        <div className="row d-flex justify-content-center mt-5">
+          <div className="carousel-div me-5">
             <Carousel>
               <Carousel.Item>
                 <img
@@ -68,22 +69,27 @@ function Product() {
             </Carousel>
           </div>
 
-          <div className="col-6">
-            <h1>Titulo</h1>
-            <small className="d-block mb-3 mt-5">USD 84</small>
-            <small className="d-block my-3">Estrellitas</small>
-            <small className="d-block my-3">Stock: 12312312 units.</small>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatem illum dolor, reprehenderit hic nulla, ad quod fugit
-              quis maxime nemo veritatis dolorem beatae excepturi asperiores
-              molestiae sunt. Eaque, dolore aspernatur?
-            </p>
-            <div className="d-flex justify-content-between">
-              <button>Add to cart</button>{" "}
-              <button className="border-0 bg-white">
-                <i class="bi bi-heart"></i>
-              </button>
+          <div className="d-flex align-items-center carousel-div">
+            <div>
+              <h1 className="">Titulo</h1>
+              <small className="d-block mb-3 mt-5">USD 84</small>
+              <small className="d-block my-3">Estrellitas</small>
+              <small className="d-block my-3">Stock: 12312312 units.</small>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem illum dolor, reprehenderit hic nulla, ad quod fugit
+                quis maxime nemo veritatis dolorem beatae excepturi asperiores
+                molestiae sunt. Eaque, dolore aspernatur?
+              </p>
+              <div className="">
+                <button className="btn fw-semibold" id="btn">
+                  Add to cart
+                </button>
+
+                <button className="border-0 bg-white ms-3">
+                  <i className="bi bi-heart"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
