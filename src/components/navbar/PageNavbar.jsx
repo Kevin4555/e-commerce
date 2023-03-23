e05437323ed7c913420b00bd98b06749f04c8fda
lemilegram <97851079+lemilegram@users.noreply.github.com>
2023-03-23 15:46:23 -0300

    Revert "Update PageNavbar.jsx"
    This reverts commit e15be389badfc352013d5f540d39eb19f0e0ee7f.

--- src/components/navbar/PageNavbar.jsx
+++ src/components/navbar/PageNavbar.jsx
@@ -2,61 +2,59 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 import "./PageNavbar.css";
 
 function PageNavbar() {
-  return (
-    <>
-      <Navbar
-        bg="white"
-        variant="light"
-        className="flex-column border-bottom"
-        sticky="top"
-      >
-        <div class="position-fixed top-0 end-0">
-          <button id="sobreEsteProyecto" className="btn">
-            <i class="bi bi-patch-question fs-3"></i>
-          </button>
-        </div>
-        <Container id="mainBar">
-          <Navbar.Brand as={Link} to={"/"}>
-            <img src="logo.png" alt="logo Manos Creativas" width="100" />
-          </Navbar.Brand>
-          <Form className="d-flex flex-grow-1 px-2">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2 flex-grow-1"
-              aria-label="Search"
-            />
-          </Form>
-          <Nav className="ms-auto fs-4">
-            <Nav.Link>
-              <i className="bi bi-person-circle"></i>
-            </Nav.Link>
-            <Nav.Link href="#pricing">
-              <i className="bi bi-cart"></i>
-            </Nav.Link>
-          </Nav>
-        </Container>
-        <Container id="categoryBar">
-          <Nav className="fs-6 flex-grow-1">
-            <Nav.Link as={Link} to={"/"}>
-              Pinturas
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/"}>
-              Cerámicas & Decoraciones
-            </Nav.Link>
-            <Nav.Link as={Link} to={"/"}>
-              Muebles & Carpintería
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
+	return (
+		<>
+			<Navbar
+				bg="white"
+				variant="light"
+				className="flex-column border-bottom"
+				sticky="top"
+			>
+				<div class="position-fixed bottom-0 end-0">
+					<button id="sobreEsteProyecto" className="btn">
+						Sobre Este Proyecto
+					</button>
+				</div>
+				<Container id="mainBar">
+					<Navbar.Brand href="/">
+						<img
+							src="logo.png"
+							alt="logo Manos Creativas"
+							width="100"
+						/>
+					</Navbar.Brand>
+					<Form className="d-flex flex-grow-1 px-2">
+						<Form.Control
+							type="search"
+							placeholder="Search"
+							className="me-2 flex-grow-1"
+							aria-label="Search"
+						/>
+					</Form>
+					<Nav className="ms-auto fs-4">
+						<Nav.Link href="#home">
+							<i className="bi bi-person-circle"></i>
+						</Nav.Link>
+						<Nav.Link href="#pricing">
+							<i className="bi bi-cart"></i>
+						</Nav.Link>
+					</Nav>
+				</Container>
+				<Container id="categoryBar">
+					<Nav className="fs-6 flex-grow-1">
+						<Nav.Link href="#home">Pinturas</Nav.Link>
+						<Nav.Link href="#home">
+							Cerámicas & Decoraciones
+						</Nav.Link>
+						<Nav.Link href="#home">Muebles & Carpintería</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</>
+	);
 }
 
 export default PageNavbar;
