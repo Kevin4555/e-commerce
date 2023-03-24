3a8298d30c92b8ff8250e38299bbf9ee7292539e
Sebastián Wajshan <sebawajshan@gmail.com>
2023-03-24 11:32:21 -0300

    sign up

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import ProductPage from "./components/pages/ProductPage/ProductPage";
 //import PageNavbar from "./components/navbar/PageNavbar";
 import Home from "./components/pages/Home/Home";
 import Cart from "./components/pages/Cart/Cart";
+import SignUp from "./components/pages/SignUp/SignUp";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/product/:id" element={<ProductPage />} />
-			<Route path="/about-us" element={<SobreEsteProyecto />} />
-			<Route path="/cart" element={<Cart />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/about-us" element={<SobreEsteProyecto />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
 }
 
 export default App;
