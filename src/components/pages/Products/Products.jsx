8b6140ad9a726e38805d7e1cd5d6061c3e5efc08
nicoroke <nicoroke@gmail.com>
2023-03-29 18:05:15 -0300

    Products, falta aplicar los filtros

--- src/components/pages/Products/Products.jsx
+++ src/components/pages/Products/Products.jsx
@@ -1,5 +1,7 @@
 import "./Products.css";
 import ProductMini from "../../ProductMini/ProductMini";
+import PageNavbar from "../../navbar/PageNavbar";
+import Carousel from "react-bootstrap/Carousel";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -67,165 +69,197 @@ function Products() {
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <Button variant="primary" onClick={handleShow}>
-              Launch demo modal
-            </Button>
+      <main>
+        <PageNavbar />
+        <Carousel className="carousel">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/mainBanner.webp"
+              alt="First slide"
+              draggable="false"
+            />
+            <Carousel.Caption className="carouselCaption">
+              <p>Lo más vendido</p>
+              <h2>Colección Madera Moderna</h2>
+              <button className="btn bannerBtn">Comprar ahora</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/img/mainBanner2.webp"
+              alt="Second slide"
+              draggable="false"
+            />
+
+            <Carousel.Caption className="carouselCaption">
+              <p>Los mejores productos hechos a mano</p>
+              <h2>Nueva Colección Handmade</h2>
+              <button className="btn bannerBtn">Comprar ahora</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-12 text-end">
+              <Button variant="btn fs-5 border rounded-pill mb-2 modal-btn" onClick={handleShow}>
+                <i class="bi bi-funnel"></i> Todos los Filtros
+              </Button>
+            </div>
+            {products.map((product) => (
+              <ProductMini product={product} key={`${product.name}_${product.id}`} />
+            ))}
           </div>
-          {products.map((product) => (
-            <ProductMini product={product} key={`${product.name}_${product.id}`} />
-          ))}
         </div>
-      </div>
-      {
-        <Modal
-          show={show}
-          onHide={handleClose}
-          dialogClassName="modal-left ms-0 m-0 p-0 vh-100 w-22rem"
-        >
-          <div className="vh-100">
-            <Modal.Header closeButton>
-              <Modal.Title>Filtros</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4 className="fs-5">Categoría</h4>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicRadio">
-                  <Form.Check
-                    type="radio"
-                    label="Todas"
-                    value="todas"
-                    checked={selectedCategoryOption === "todas"}
-                    onChange={handleOptionChangeCategory}
-                  />
-                  {categories.map((category) => {
-                    return (
-                      <Form.Check
-                        type="radio"
-                        label={category.name}
-                        value={category.id}
-                        checked={selectedCategoryOption === category.id}
-                        onChange={handleOptionChangeCategory}
-                        key={`${category.name}_${category.id}`}
-                      />
-                    );
-                  })}
-                </Form.Group>
-              </Form>
-              <h4 className="fs-5">Calificación</h4>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicRadio">
-                  <Form.Check
-                    type="radio"
-                    label="Una estrella"
-                    value="unaEstrella"
-                    checked={selectedRatingOption === "unaEstrella"}
-                    onChange={handleOptionChangeRating}
-                  />
+        {
+          <Modal
+            show={show}
+            onHide={handleClose}
+            dialogClassName="modal-left ms-0 m-0 p-0 vh-100 w-22rem"
+          >
+            <div className="vh-100">
+              <Modal.Header closeButton>
+                <Modal.Title>Filtros</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h4 className="fs-5">Categoría</h4>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicRadio">
+                    <Form.Check
+                      type="radio"
+                      label="Todas"
+                      value="todas"
+                      checked={selectedCategoryOption === "todas"}
+                      onChange={handleOptionChangeCategory}
+                    />
+                    {categories.map((category) => {
+                      return (
+                        <Form.Check
+                          type="radio"
+                          label={category.name}
+                          value={category.id}
+                          checked={selectedCategoryOption === category.id}
+                          onChange={handleOptionChangeCategory}
+                          key={`${category.name}_${category.id}`}
+                        />
+                      );
+                    })}
+                  </Form.Group>
+                </Form>
+                <h4 className="fs-5">Calificación</h4>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicRadio">
+                    <Form.Check
+                      type="radio"
+                      label="Una estrella"
+                      value="unaEstrella"
+                      checked={selectedRatingOption === "unaEstrella"}
+                      onChange={handleOptionChangeRating}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="Dos estrellas"
-                    value="dosEstrella"
-                    checked={selectedRatingOption === "dosEstrella"}
-                    onChange={handleOptionChangeRating}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="Dos estrellas"
+                      value="dosEstrella"
+                      checked={selectedRatingOption === "dosEstrella"}
+                      onChange={handleOptionChangeRating}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="Tres estrellas"
-                    value="tresEstrellas"
-                    checked={selectedRatingOption === "tresEstrellas"}
-                    onChange={handleOptionChangeRating}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="Tres estrellas"
+                      value="tresEstrellas"
+                      checked={selectedRatingOption === "tresEstrellas"}
+                      onChange={handleOptionChangeRating}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="Cuatro estrellas"
-                    value="cuatroEstrellas"
-                    checked={selectedRatingOption === "cuatroEstrellas"}
-                    onChange={handleOptionChangeRating}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="Cuatro estrellas"
+                      value="cuatroEstrellas"
+                      checked={selectedRatingOption === "cuatroEstrellas"}
+                      onChange={handleOptionChangeRating}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="Cinco estrellas"
-                    value="cincoEstrellas"
-                    checked={selectedRatingOption === "cincoEstrellas"}
-                    onChange={handleOptionChangeRating}
-                  />
-                </Form.Group>
-              </Form>
-              <h4 className="fs-5">Précio</h4>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicRadio">
-                  <Form.Check
-                    type="radio"
-                    label="Cualquier precio"
-                    value="anyPrice"
-                    checked={selectedPriceOption === "anyPrice"}
-                    onChange={handleOptionChangePrice}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="Cinco estrellas"
+                      value="cincoEstrellas"
+                      checked={selectedRatingOption === "cincoEstrellas"}
+                      onChange={handleOptionChangeRating}
+                    />
+                  </Form.Group>
+                </Form>
+                <h4 className="fs-5">Précio</h4>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicRadio">
+                    <Form.Check
+                      type="radio"
+                      label="Cualquier precio"
+                      value="anyPrice"
+                      checked={selectedPriceOption === "anyPrice"}
+                      onChange={handleOptionChangePrice}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="Menor de USD 25"
-                    value="menorUSD25"
-                    checked={selectedPriceOption === "menorUSD25"}
-                    onChange={handleOptionChangePrice}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="Menor de USD 25"
+                      value="menorUSD25"
+                      checked={selectedPriceOption === "menorUSD25"}
+                      onChange={handleOptionChangePrice}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="USD 25 a USD 50"
-                    value="USD25aUSD50"
-                    checked={selectedPriceOption === "USD25aUSD50"}
-                    onChange={handleOptionChangePrice}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="USD 25 a USD 50"
+                      value="USD25aUSD50"
+                      checked={selectedPriceOption === "USD25aUSD50"}
+                      onChange={handleOptionChangePrice}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="USD 50 a USD 100"
-                    value="USD50aUSD100"
-                    checked={selectedPriceOption === "USD50aUSD100"}
-                    onChange={handleOptionChangePrice}
-                  />
+                    <Form.Check
+                      type="radio"
+                      label="USD 50 a USD 100"
+                      value="USD50aUSD100"
+                      checked={selectedPriceOption === "USD50aUSD100"}
+                      onChange={handleOptionChangePrice}
+                    />
 
-                  <Form.Check
-                    type="radio"
-                    label="Más de 100"
-                    value="masDe100"
-                    checked={selectedPriceOption === "masDe100"}
-                    onChange={handleOptionChangePrice}
-                  />
-                  <Form.Check
-                    type="radio"
-                    label="Personalizados"
-                    value="personalizados"
-                    checked={selectedPriceOption === "personalizados"}
-                    onChange={handleOptionChangePrice}
-                  />
-                  <div className="d-flex">
-                    <Form.Control type="number" placeholder="Mínimo" className="w-6rem" />
-                    <small className="my-auto fs-6 mx-2">To</small>
-                    <Form.Control type="number" placeholder="Máximo" className="w-6rem" />
-                  </div>
-                </Form.Group>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer className="justify-content-start">
-              <Button className="w-6rem" variant="secondary" onClick={handleClose}>
-                Cerrar
-              </Button>
-              <Button className="w-6rem" variant="primary" onClick={handleClose}>
-                Aplicar
-              </Button>
-            </Modal.Footer>
-          </div>
-        </Modal>
-      }
+                    <Form.Check
+                      type="radio"
+                      label="Más de 100"
+                      value="masDe100"
+                      checked={selectedPriceOption === "masDe100"}
+                      onChange={handleOptionChangePrice}
+                    />
+                    <Form.Check
+                      type="radio"
+                      label="Personalizados"
+                      value="personalizados"
+                      checked={selectedPriceOption === "personalizados"}
+                      onChange={handleOptionChangePrice}
+                    />
+                    <div className="d-flex">
+                      <Form.Control type="number" placeholder="Mínimo" className="w-6rem" />
+                      <small className="my-auto fs-6 mx-2">To</small>
+                      <Form.Control type="number" placeholder="Máximo" className="w-6rem" />
+                    </div>
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer className="justify-content-start">
+                <Button className="w-6rem" variant="secondary" onClick={handleClose}>
+                  Cerrar
+                </Button>
+                <Button className="w-6rem" variant="primary" onClick={handleClose}>
+                  Aplicar
+                </Button>
+              </Modal.Footer>
+            </div>
+          </Modal>
+        }
+      </main>
     </>
   );
 }
