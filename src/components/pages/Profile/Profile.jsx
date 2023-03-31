34f17cc031cc4e426baf8570039007167f393c0e
nicoroke <nicoroke@gmail.com>
2023-03-31 11:13:51 -0300

    Footer y newsletter agregados a "Category", y "Profile", profile responsive

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -3,6 +3,8 @@ import PageNavbar from "../../navbar/PageNavbar";
 import ProductMini from "../../ProductMini/ProductMini";
 import MultiItemCarousel from "../../Carousel/MultiItemCarousel";
 import Order from "../../Order/Order";
+import Newsletter from "../../Newsletter/Newsletter";
+import Footer from "../../Footer/Footer";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -33,18 +35,8 @@ function Profile() {
         </div>
         <div className="container mt-5">
           <div className="row">
-            <div className="col-8">
-              <h2 className="fs-4">Favoritos</h2>
-              {products && (
-                <MultiItemCarousel
-                  products={products.filter((product) => (product.rating = 5))}
-                  productsPerPage={2}
-                  className=""
-                />
-              )}
-            </div>
-            <div className="col-4">
-              <div className="text-center bg-secondary-subtle p-4 rounded">
+            <div className="d-lg-none d-block">
+              <div className="text-center bg-secondary-subtle p-4 rounded vh-100">
                 <img
                   src="/img/default-avatar.jpg"
                   alt="Profile"
@@ -52,43 +44,94 @@ function Profile() {
                 />
                 <h2 className="mb-0 mt-3">Pepe Rodriguez</h2>
                 <small>pepe@gmail.com</small>
-                <div className="row mt-5 bg-white p-2">
-                  <div className="col-6 text-start my-2">
+                <div className="row mt-5 bg-white p-5 p-lg-2">
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fw-bold fs-6">Firstname: </small>
                     <small className="fs-6">Pepe</small>
                   </div>
-                  <div className="col-6 text-start my-2">
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fw-bold fs-6">Lastname: </small>
                     <small className="fs-6">Rodriguez</small>
                   </div>
-                  <div className="col-6 text-start my-2">
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fs-6 fw-bold">Phone: </small>
                     <small className="fs-6">091000000</small>
                   </div>
-                  <div className="col-6 text-start my-2">
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fw-bold fs-6">Birthdate: </small>
                     <small className="fs-6">1/1/1994</small>
                   </div>
-                  <div className="col-12 text-start my-2">
+                  <div className="col-12 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fw-bold fs-6">Avatar: </small>
                     <small className="fs-6">default-avatar.jpg</small>
                   </div>
-                  <div className="col-12 text-start my-2">
+                  <div className="col-12 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fw-bold fs-6">Email: </small>
                     <small className="fs-6">pepe@gmail.com</small>
                   </div>
-                  <div className="col-12 text-start my-2">
+                  <div className="col-12 d-lg-flex flex-lg-column text-start my-2">
                     <small className="fw-bold fs-6">Address: </small>
                     <small className="fs-6">Br. Artigas 2222</small>
                   </div>
                 </div>
               </div>
             </div>
-            <h2 className="fs-3 mt-5">Historial de pedidos</h2>
-            <Order />
+            <div className="col-12 col-lg-8">
+              <h2 className="fs-3">Historial de pedidos</h2>
+              <Order />
+              {/* <h2 className="fs-4">Favoritos</h2>
+              {products && (
+                <MultiItemCarousel products={products} productsPerPage={4} className="" />
+              )} */}
+            </div>
+            <div className="d-none d-lg-block col-lg-4">
+              <div className="text-center bg-secondary-subtle p-4 rounded vh-100">
+                <img
+                  src="/img/default-avatar.jpg"
+                  alt="Profile"
+                  className="profile-img rounded-pill"
+                />
+                <h2 className="mb-0 mt-3">Pepe Rodriguez</h2>
+                <small>pepe@gmail.com</small>
+                <div className="row mt-5 bg-white p-5 p-lg-2">
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fw-bold fs-6">Firstname: </small>
+                    <small className="fs-6">Pepe</small>
+                  </div>
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fw-bold fs-6">Lastname: </small>
+                    <small className="fs-6">Rodriguez</small>
+                  </div>
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fs-6 fw-bold">Phone: </small>
+                    <small className="fs-6">091000000</small>
+                  </div>
+                  <div className="col-6 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fw-bold fs-6">Birthdate: </small>
+                    <small className="fs-6">1/1/1994</small>
+                  </div>
+                  <div className="col-12 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fw-bold fs-6">Avatar: </small>
+                    <small className="fs-6">default-avatar.jpg</small>
+                  </div>
+                  <div className="col-12 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fw-bold fs-6">Email: </small>
+                    <small className="fs-6">pepe@gmail.com</small>
+                  </div>
+                  <div className="col-12 d-lg-flex flex-lg-column text-start my-2">
+                    <small className="fw-bold fs-6">Address: </small>
+                    <small className="fs-6">Br. Artigas 2222</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*           <h2 className="fs-3 mt-5">Historial de pedidos</h2>
+            <Order /> */}
           </div>
         </div>
       </main>
+      <Newsletter />
+      <Footer />
     </>
   );
 }
