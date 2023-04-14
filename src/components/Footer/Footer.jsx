0f27889839df38a480813da9c64a34902bc7eedc
KChino <kevintlapizaga@gmail.com>
2023-04-14 10:26:53 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,11 @@ function Footer() {
         <nav className="row">
           <div className="col-2 d-none d-md-block"></div>
           <div className="col-12 col-md-4">
-            <img src={`${process.env.REACT_APP_API_BASE_IMG_URL}/logo.png`} className="w-50 ms-2" />
+            <img
+              src={`${process.env.REACT_APP_API_BASE_IMG_URL}/logo.png`}
+              alt="Logo"
+              className="w-50 ms-2"
+            />
             <div className={`${css.socials} ms-4`}>
               <i className={`bi bi-instagram ${css.bi}`} />
               <i className={`bi bi-facebook ${css.bi}`} />
